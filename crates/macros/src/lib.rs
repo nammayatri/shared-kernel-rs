bb9a43c82b4299c41d6674a4bb4701355df708ed
khuzema786 <khomosikhuzema894@gmail.com>
2024-05-26 17:12:22 +0530

    backend/enhancement: Measure duration for sync and async function both

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -17,16 +17,31 @@ pub fn measure_duration(_: TokenStream, input: TokenStream) -> TokenStream {
     let fn_name = &input_fn.sig.ident;
     let args = &input_fn.sig.inputs;
     let return_type = &input_fn.sig.output;
+    let is_async = input_fn.sig.asyncness.is_some();
 
-    let expanded = quote! {
-        pub async fn #fn_name(#args) #return_type {
-            let start_time = std::time::Instant::now();
-            let result = #function_body;
-            measure_latency_duration!(stringify!(#fn_name), start_time);
-            let elapsed_time = start_time.elapsed();
-            let elapsed_ms = elapsed_time.as_secs() * 1000 + u64::from(elapsed_time.subsec_millis());
-            debug!("Function: {} | Duration (ms): {}", stringify!(#fn_name), elapsed_ms);
-            result
+    let expanded = if is_async {
+        quote! {
+            pub async fn #fn_name(#args) #return_type {
+                let start_time = std::time::Instant::now();
+                let result = #function_body;
+                measure_latency_duration!(stringify!(#fn_name), start_time);
+                let elapsed_time = start_time.elapsed();
+                let elapsed_ms = elapsed_time.as_secs() * 1000 + u64::from(elapsed_time.subsec_millis());
+                debug!("Function: {} | Duration (ms): {}", stringify!(#fn_name), elapsed_ms);
+                result
+            }
+        }
+    } else {
+        quote! {
+            pub fn #fn_name(#args) #return_type {
+                let start_time = std::time::Instant::now();
+                let result = #function_body;
+                measure_latency_duration!(stringify!(#fn_name), start_time);
+                let elapsed_time = start_time.elapsed();
+                let elapsed_ms = elapsed_time.as_secs() * 1000 + u64::from(elapsed_time.subsec_millis());
+                debug!("Function: {} | Duration (ms): {}", stringify!(#fn_name), elapsed_ms);
+                result
+            }
         }
     };
 
